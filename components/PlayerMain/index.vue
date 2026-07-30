c4482d86485691411ee93d6900f3cac3f3c4c80a
alizoka <ali.zokaei.1367@gmail.com>
2026-07-30 12:30:37 +0400

    change PLAYBACK_TIMEOUT_MS  to 6 seconds

--- components/PlayerMain/index.vue
+++ components/PlayerMain/index.vue
@@ -116,7 +116,7 @@ function getRandomNumberSupport() {
 }
 
 
-const PLAYBACK_TIMEOUT_MS = 3000
+const PLAYBACK_TIMEOUT_MS = 6000
 
 let playerInitResolve = null
 const playerInitPromise = new Promise((resolve) => {
